c0870fe9a2b2566319d44f60c9cdc100247d44cc
Bruno Roy <brusi_roy@hotmail.com>
2018-04-10 15:54:08 -0400

    Add the bank datasets

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,10 @@
 from sklearn.model_selection import train_test_split
 import time
+import pandas as pd
 
 from sklearn import datasets, svm, metrics
-from utils import get_MNIST
+from sklearn.preprocessing import LabelEncoder
+from utils import get_MNIST, get_bank, get_bank_full
 from sklearn import datasets, metrics
 from support_vector_machine import support_vector_machine
 from random_forest import random_forest
@@ -10,60 +12,68 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
 
 # MNIST benchmark
-data, target = get_MNIST()
-X_train, X_test, y_train, y_test = train_test_split(data / 255., target, train_size=60000, stratify=target, random_state=0)
+#data, target = get_MNIST()
+#X_train, X_test, y_train, y_test = train_test_split(data / 255., target, train_size=60000, stratify=target, random_state=0)
+
+
+data, target = get_bank_full()
+
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1, stratify=target, random_state=0)
 
 
 def benchmark(classifier, dataset_name, X_train, X_test, y_train, y_test):
+    """Benchmark the classifier"""
 
-    start_time = time.time()
-    # Fitting
-    classifier.fit(X_train, y_train)
+    if classifier.__class__.__name__ == 'MLPClassifier':
+        start_time = time.time()
+        # Fitting
+        classifier.fit(X_train, y_train)
 
-    expected = y_test
-    predicted = classifier.predict(X_test)
+        train_score = classifier.score(X_train, y_train)
+        test_score = classifier.score(X_test, y_test)
 
-    total_time = time.time() - start_time
+        total_time = time.time() - start_time
 
-    with open(f"results/{classifier.__class__.__name__}/{dataset_name}-{start_time}.txt", 'a') as f:
-        f.write(f'Result for the {classifier.__class__.__name__} on the {dataset_name} dataset with the following specification:\n')
-        f.write(str(classifier))
-        f.write('Classification Report:\n')
-        f.write(str(metrics.classification_report(expected, predicted)))
-        f.write('Confusion matrix:\n')
-        f.write(str(metrics.confusion_matrix(expected, predicted)))
-        f.write(f'\nExecution time: {total_time}')
+        with open(f"results/{classifier.__class__.__name__}/{dataset_name}-{start_time}.txt", 'a') as f:
+            f.write(f'Result for the random forest on the {dataset_name} dataset with the following specification:\n')
+            f.write(str(classifier))
+            f.write(f'\nTrain score: {train_score}\n')
+            f.write(f'Test score: {test_score}\n')
+            f.write(f'\nExecution time: {total_time}')
+    else:
+        start_time = time.time()
+        # Fitting
+        classifier.fit(X_train, y_train)
 
-def MLP_benchmark(classifier, dataset_name, X_train, X_test, y_train, y_test):
-    start_time = time.time()
-    # Fitting
-    classifier.fit(X_train, y_train)
+        expected = y_test
+        predicted = classifier.predict(X_test)
 
-    train_score = classifier.score(X_train, y_train)
-    test_score = classifier.score(X_test, y_test)
+        total_time = time.time() - start_time
 
-    total_time = time.time() - start_time
+        with open(f"results/{classifier.__class__.__name__}/{dataset_name}-{start_time}.txt", 'a') as f:
+            f.write(f'Result for the {classifier.__class__.__name__} on the {dataset_name} dataset with the following specification:\n')
+            f.write(str(classifier))
+            f.write('Classification Report:\n')
+            f.write(str(metrics.classification_report(expected, predicted)))
+            f.write('Confusion matrix:\n')
+            f.write(str(metrics.confusion_matrix(expected, predicted)))
+            f.write(f'\nExecution time: {total_time}')
 
-    with open(f"results/{classifier.__class__.__name__}/{dataset_name}-{start_time}.txt", 'a') as f:
-        f.write(f'Result for the random forest on the {dataset_name} dataset with the following specification:\n')
-        f.write(str(classifier))
-        f.write(f'\nTrain score: {train_score}\n')
-        f.write(f'Test score: {test_score}\n')
-        f.write(f'\nExecution time: {total_time}')
-
-
-
-rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=0)
-benchmark(rf, 'MNIST', X_train, X_test, y_train, y_test)
+#rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=0)
+#benchmark(rf, 'MNIST', X_train, X_test, y_train, y_test)
+#benchmark(rf, 'Bank', X_train, X_test, y_train, y_test)
 
 #svm = svm.SVC(gamma=0.001)
 #benchmark(svm, 'MNIST', X_train, X_test, y_train, y_test)
+#benchmark(svm, 'Bank', X_train, X_test, y_train, y_test)
 
 #gb = GradientBoostingClassifier(n_estimators=10, learning_rate=1.0, max_depth=1, random_state=0)
 #benchmark(gb, 'MNIST', X_train, X_test, y_train, y_test)
+
 
 mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10, alpha=1e-4,
                     solver='sgd', verbose=10, tol=1e-4, random_state=1,
                     learning_rate_init=.1)
 
-#MLP_benchmark(mlp, 'MNIST', X_train, X_test, y_train, y_test)
+benchmark(mlp, 'Bank-full', X_train, X_test, y_train, y_test)
--- utils.py
+++ utils.py
@@ -1,6 +1,37 @@
 from sklearn.datasets import fetch_mldata
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
 
 def get_MNIST():
-    """Returns a (data, target) tuple of the MNIST dataset"""
+    """Returns a (data, target) tuple of the MNIST dataset (70 000 items)"""
     mnist = fetch_mldata('MNIST original', data_home='./data/')
     return (mnist.data, mnist.target)
+
+
+def get_bank():
+    """Returns a (data, target) tuple of the Bank dataset (4521 items)"""
+    df = pd.read_csv('data/bank/bank.csv', sep=';')
+
+    for column in df:
+        enc = LabelEncoder()
+        enc.fit(df[column])
+        df[column] = enc.transform(df[column])
+
+    data = df.loc[:, df.columns != 'y']
+    target = df.loc[:, df.columns == 'y']
+    return (data, target)
+
+
+def get_bank_full():
+    """Returns a (data, target) tuple of the Bank-full dataset (45211 items)"""
+    df = pd.read_csv('data/bank/bank-full.csv', sep=';')
+
+    for column in df:
+        enc = LabelEncoder()
+        enc.fit(df[column])
+        df[column] = enc.transform(df[column])
+
+    data = df.loc[:, df.columns != 'y']
+    target = df.loc[:, df.columns == 'y']
+    return (data, target)
